5b282e50b6418cac64621171a466995b52962c6a
BFMapper <144379241+BFMapper@users.noreply.github.com>
2023-11-26 18:22:44 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,4 @@
+alert("welcome to my website, here you can watch my videos if you want :)");
+window.addEventListener("load", function () {
+  document.querySelector("body").classList.add("loaded");
+});
